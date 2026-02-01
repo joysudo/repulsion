b351dc32af6ed6aa59141c36c2d575c017d6ee3a
Joy <joysu2008@gmail.com>
2026-02-02 03:15:14 +0800

    parallax and camera zoom and template scenes

--- camera_2d.gd
+++ camera_2d.gd
@@ -4,19 +4,21 @@ extends Camera2D
 @onready var p2 = $"../Player2"
 @onready var tilemap = $"../TileMapLayer"
 
-const min_zoom = 0.8
+const min_zoom = 0.7
 const zoom_threshold = 150.0
 const max_dist = 1700.0
 const default_zoom = 1.2
 
 func _ready() -> void:
+	print(p1)
+	print(p2)
 	if tilemap:
 		var map_rect = tilemap.get_used_rect()
 		var tile_size = tilemap.tile_set.tile_size
-		limit_left = map_rect.position.x * tile_size.x
-		limit_right = map_rect.end.x * tile_size.x
-		limit_top = map_rect.position.y * tile_size.y
-		limit_bottom = map_rect.end.y * tile_size.y
+		limit_left = map_rect.position.x * tile_size.x - 100
+		limit_right = map_rect.end.x * tile_size.x + 100
+		limit_top = map_rect.position.y * tile_size.y - 100
+		limit_bottom = map_rect.end.y * tile_size.y + 100
 		offset = Vector2.ZERO
 
 func _process(_delta: float) -> void:
@@ -28,7 +30,6 @@ func _process(_delta: float) -> void:
 		var zoom_target = default_zoom
 		if dist > zoom_threshold:
 			zoom_target = remap(dist, zoom_threshold, max_dist, default_zoom, min_zoom)
-		
 		zoom_target = clamp(zoom_target, min_zoom, default_zoom)
 		zoom = zoom.lerp(Vector2(zoom_target, zoom_target), 0.05)
 	elif is_instance_valid(p2):
