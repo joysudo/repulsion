a474637ad43a51d7a5cc39d0fd5aeb57b32afe67
Joy <joysu2008@gmail.com>
2026-02-02 01:43:19 +0800

    camera zooms out when players far apart

--- camera_2d.gd
+++ camera_2d.gd
@@ -2,9 +2,36 @@ extends Camera2D
 
 @onready var p1 = $"../Player1"
 @onready var p2 = $"../Player2"
+@onready var tilemap = $"../TileMapLayer"
+
+const min_zoom = 0.8
+const zoom_threshold = 150.0
+const max_dist = 1700.0
+const default_zoom = 1.2
+
+func _ready() -> void:
+	if tilemap:
+		var map_rect = tilemap.get_used_rect()
+		var tile_size = tilemap.tile_set.tile_size
+		limit_left = map_rect.position.x * tile_size.x
+		limit_right = map_rect.end.x * tile_size.x
+		limit_top = map_rect.position.y * tile_size.y
+		limit_bottom = map_rect.end.y * tile_size.y
+		offset = Vector2.ZERO
 
 func _process(_delta: float) -> void:
 	if is_instance_valid(p1) and is_instance_valid(p2):
-		global_position = (p1.global_position + p2.global_position) / 2.0
-		global_position.y -= 130
-		global_position.x += 100
+		var target_pos = (p1.global_position + p2.global_position) / 2.0
+		global_position = global_position.lerp(target_pos, 0.1)
+		# zoom
+		var dist = p1.global_position.distance_to(p2.global_position)
+		var zoom_target = default_zoom
+		if dist > zoom_threshold:
+			zoom_target = remap(dist, zoom_threshold, max_dist, default_zoom, min_zoom)
+		
+		zoom_target = clamp(zoom_target, min_zoom, default_zoom)
+		zoom = zoom.lerp(Vector2(zoom_target, zoom_target), 0.05)
+	elif is_instance_valid(p2):
+		global_position = global_position.lerp(p2.global_position, 0.1)
+	elif is_instance_valid(p1):
+		global_position = global_position.lerp(p2.global_position, 0.1)
